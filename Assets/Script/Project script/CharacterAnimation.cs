5c1ac435c02255e418f8e8f69539479d7456191e
Tan Zhi Quan <65937023+zhiquantan@users.noreply.github.com>
2022-06-12 00:56:34 +0800

    Added fps and ray caster to detect object

---
+++ Assets/Script/Project script/CharacterAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    private Animator animator;
+    public float speed;
+    public GameObject FPS;
+
+    void Start()
+    {
+        animator=GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        // Debug.Log(FPS.GetComponent<SC_FPSController>().curSpeedY);
+        
+        if(FPS.GetComponent<SC_FPSController>().curSpeedX==0&&FPS.GetComponent<SC_FPSController>().curSpeedY==0)
+        {
+            speed=0;
+        }
+
+        else{
+            speed=1;
+        }
+
+        animator.SetFloat("Speed",speed);
+
+        
+    }
+}
---
+++ Assets/Script/Project script/RayCaster.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCaster : MonoBehaviour
+{
+    public GameObject Hit;
+    public GameObject AnimalIcon;
+    public GameObject NothingIcon;
+    public GameObject MaterialIcon;
+    
+    void FixedUpdate()
+    {
+        var ray = new Ray(transform.position, this.transform.TransformDirection(Vector3.forward));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit,8))
+        {
+            //Debug.DrawRay(transform.position, this.transform.TransformDirection(Vector3.forward)*hit.distance,Color.red);
+             Hit = hit.transform.gameObject;
+
+            if (Hit.tag == "Material")
+            {
+
+                MaterialIcon.GetComponent<SpriteRenderer>().enabled = true;
+                NothingIcon.GetComponent<SpriteRenderer>().enabled = false;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                   Debug.Log("M");
+                }
+            }
+
+            else if (Hit.tag == "Animal")
+            {
+
+                AnimalIcon.GetComponent<SpriteRenderer>().enabled = true;
+                NothingIcon.GetComponent<SpriteRenderer>().enabled = false;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("A");
+                }
+            }
+
+            else
+            {
+                AnimalIcon.GetComponent<SpriteRenderer>().enabled = false;
+                MaterialIcon.GetComponent<SpriteRenderer>().enabled = false;
+                NothingIcon.GetComponent<SpriteRenderer>().enabled = true;
+            }
+        }
+    }
+}
